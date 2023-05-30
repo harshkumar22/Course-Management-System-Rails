6f88550bebe700e325bbd168d758511c0186a99c
Harsh Kumar <harsh.kumar2@comviva.com>
2023-05-30 12:02:26 +0530

    Owner can't by there own course

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,11 +10,16 @@ class TransactionsController < ApplicationController
     def create
         if CourseEnrollment.find_by(user_id: params[:user_id], course_id: params[:course_id]) == nil
             @transaction = Transaction.new(user_id: params[:user_id], course_id: params[:course_id], total_amount: params[:total_amount], payment_status: params[:payment_status])
-            if @transaction.save
+            coursepublisher = CoursePublish.find_by(course_id: params[:course_id]).user_id
+            user_session_id = session[:user_id]
+            user = User.find(user_session_id)
+
+            if coursepublisher == user.id
+                flash[:notice] = "Owner can't but there own course"
+                render 'new'
+            elsif @transaction.save
                 flash[:notice] = "Payment Succesfull, With Transaction ID: #{@transaction.id}"
-                if @transaction.payment_status == true
-                    user_session_id = session[:user_id]
-                    user = User.find(user_session_id)
+                if @transaction.payment_status == true and 
                     valid_till = Course.find(params[:course_id]).validity
                     @course_enrollment = CourseEnrollment.create(user_id: user.id, course_id: @transaction.course_id, valid_till: DateTime.now().next_day(valid_till))
                     @course_enrollment.save
